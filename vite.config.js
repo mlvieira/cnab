b90ae4ef905fca7abf28068740fe9509d0c9f2ba
mlvieira <54179980+mlvieira@users.noreply.github.com>
2025-05-07 13:34:47 -0300

    cleanup

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tailwindcss from '@tailwindcss/vite'
 
-const repositoryName = "cnab";
-
 // https://vite.dev/config/
 export default defineConfig({
   base: 'cnab/',
