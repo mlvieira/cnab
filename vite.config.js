005821c0534c899599aaa3724e85fb0aa8d82d84
mlvieira <54179980+mlvieira@users.noreply.github.com>
2025-05-07 09:38:17 -0300

    Configure Vite and add GitHub Actions for Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,13 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tailwindcss from '@tailwindcss/vite'
 
+const repositoryName = "cnab";
+
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    base: process.env.NODE_ENV === "production" ? `/${repositoryName}/` : "./",
+  },
   plugins: [
     svelte(),
     tailwindcss()
