782c1e844f4b22b04a12373c7b458e896e0e162f
mlvieira <54179980+mlvieira@users.noreply.github.com>
2025-05-07 13:28:55 -0300

    fix workflow

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,7 @@ const repositoryName = "cnab";
 
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    base: process.env.NODE_ENV === "production" ? `/${repositoryName}/` : "./",
-  },
+  base: process.env.NODE_ENV === "production" ? `/${repositoryName}` : "./",
   plugins: [
     svelte(),
     tailwindcss()
