9d308c201dd2bba9115c73025b98badcad455c72
mlvieira <54179980+mlvieira@users.noreply.github.com>
2025-05-07 13:16:21 -0300

    github pages

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,10 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tailwindcss from '@tailwindcss/vite'
 
-const repositoryName = "cnab";
-
 // https://vite.dev/config/
 export default defineConfig({
   build: {
-    base: process.env.NODE_ENV === "production" ? `/${repositoryName}/` : "./",
+    base: '/cnab',
   },
   plugins: [
     svelte(),
