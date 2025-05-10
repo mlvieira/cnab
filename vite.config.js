0613022f36d5e68ddeee9e3c346227098f6e8193
mlvieira <54179980+mlvieira@users.noreply.github.com>
2025-05-10 16:46:21 -0300

    add slash in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'cnab/',
+  base: '/cnab/',
   plugins: [
     svelte(),
     tailwindcss()
